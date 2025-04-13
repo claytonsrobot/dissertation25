1370609d987359ab965d4dcf77a3d144e846c450
Clayton Bennett <claytonrobot@gmail.com>
2025-04-12 19:44:42 -0500

    gitignore

---
+++ utils/foldersizes.ps1
@@ -0,0 +1,10 @@
+$folderPath = "C:\Users\user\Documents\dev\dissertation25\.venv\Lib\site-packages"
+Get-ChildItem -Directory -Path $folderPath | ForEach-Object {
+    $folder = $_
+    $size = (Get-ChildItem -Recurse -Path $folder.FullName | Measure-Object -Property Length -Sum).Sum
+    [PSCustomObject]@{
+        FolderName = $folder.Name
+        SizeInBytes = $size
+        SizeInMB = "{0:N2}" -f ($size / 1MB)
+    }
+} | Format-Table -AutoSize
